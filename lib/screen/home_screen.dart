07ea86038afa10b6ba7c4b22a14297e8d9624805
RayaAlam <alamraya248@gmail.com>
2025-02-11 13:36:47 +0700

    menampilkan postingan di home

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_uilogin/screen/create_post_screen.dart';
@@ -8,30 +11,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'detail_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    // Data dummy untuk contoh
-    final List<Map<String, dynamic>> pins = [
-      {
-        'imageUrl': 'https://picsum.photos/id/1/400/600',
-        'title': 'Workspace Setup',
-        'height': 300.0,
-      },
-      {
-        'imageUrl': 'https://picsum.photos/id/2/400/300',
-        'title': 'Modern Design',
-        'height': 200.0,
-      },
-      {
-        'imageUrl': 'https://picsum.photos/id/3/400/500',
-        'title': 'Creative Space',
-        'height': 250.0,
-      },
-    ];
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: CustomScrollView(
@@ -39,42 +28,57 @@ class HomeScreen extends StatelessWidget {
           SliverAppBar(
             floating: true,
             title: Text('Cobaterest',
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
             backgroundColor: Colors.black,
             actions: [
               IconButton(
                   onPressed: () {
                     FirebaseAuth.instance.signOut();
                   },
-                  icon: const Icon(Icons.logout, color: Colors.white)
-              ),
+                  icon: const Icon(Icons.logout, color: Colors.white)),
             ],
           ),
           SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            sliver: SliverMasonryGrid.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 8,
-              crossAxisSpacing: 8,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailScreen(pin: pins[index % pins.length]),
-                      ),
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              sliver: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection('posts')
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    return SliverMasonryGrid(
+                      gridDelegate:
+                          SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2),
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                      delegate: SliverChildBuilderDelegate((context, index) {
+                        final data = snapshot.data!.docs[index];
+
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailScreen(
+                                    pin: <String, dynamic>{
+                                      "title": data["title"],
+                                      "imageUrl": "https://picsum.photos/100/100",
+                                    }
+                                  ),
+                              ),
+                            );
+                          },
+                          child: PinCard(
+                            imageUrl: "https://picsum.photos/100/100",
+                            title: data["title"],
+                            height: 400,
+                          ),
+                        );
+                      }, childCount: snapshot.data?.size ?? 0),
                     );
-                  },
-                  child: PinCard(
-                    imageUrl: pins[index % pins.length]['imageUrl'],
-                    title: pins[index % pins.length]['title'],
-                    height: pins[index % pins.length]['height'],
-                  ),
-                );
-              },
-            ),
-          ),
+                  })),
         ],
       ),
     );
